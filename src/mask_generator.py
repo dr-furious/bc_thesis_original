61895ce37519031430b33ba5f95ef979b90b1bce
Martin Sivak <xsivakm1@stuba.sk>
2025-04-18 15:35:37 +0200

    feat: implement combine method that combines small rois into large png mask

--- src/mask_generator.py
+++ src/mask_generator.py
@@ -1,8 +1,9 @@
-import json
 import os
+import cv2
 import numpy as np
 from typing import List, Tuple, Dict, Union, Callable
 from PIL import Image
+from matplotlib import pyplot as plt
 
 from src.utils import load_images, load_json
 
@@ -35,6 +36,29 @@ def _cut(image_path: str, regions: List[List[int]], out_dir: str | None = None) 
     return cut_roi_images
 
 
+def _check_bbox(roi: np.ndarray, bbox: Tuple[int, int, int, int], image_shape: Tuple[int, int]) \
+        -> Tuple[int, int, int, int, np.ndarray]:
+    x, y, w, h = bbox
+    print(f"x, y, w, h: {x, y, w, h}")
+    if x < 0:
+        roi = roi[:, abs(x):]
+        w = w + x
+        x = 0
+    if y < 0:
+        roi = roi[abs(y):, :]
+        h = h + y
+        y = 0
+
+    if x + w > image_shape[1]:
+        roi = roi[:, :-(x + w - image_shape[1])]
+        w = image_shape[1] - x
+    if y + h > image_shape[0]:
+        roi = roi[:-(y + h - image_shape[0]), :]
+        h = image_shape[0] - y
+
+    return x, y, w, h, roi
+
+
 class MaskGenerator:
     images_dir: str | None
     images: List[Tuple[np.ndarray, str]]
@@ -49,7 +73,7 @@ class MaskGenerator:
         self.images = load_images(images_dir, supported_formats)
 
     def cut_cells(self, json_path: str, out_dir: str | None = None) \
-            -> List[Dict[str, Union[str, dict]]]:
+            -> List[Dict[str, Union[str, List[dict]]]]:
         data = load_json(json_path)
         images = data['images']
         cut_results = []
@@ -67,24 +91,57 @@ class MaskGenerator:
             })
         return cut_results
 
-    def generate_masks(self, image_data: List[Dict[str, Union[str, dict]]], operations: List[Callable]) -> None:
+    def generate_masks(self, image_data: List[Dict[str, Union[str, List[dict]]]], operations: List[Callable]) -> None:
         for operation in operations:
             image_data = operation(image_data)
 
-    def otsu_thresholding(self, image_data: List[Dict[str, Union[str, dict]]]):
+    def otsu_thresholding(self, image_data: List[Dict[str, Union[str, List[dict]]]]):
         # your code here
         return image_data
 
-    def open_close(self, image_data: List[Dict[str, Union[str, dict]]]):
+    def open_close(self, image_data: List[Dict[str, Union[str, List[dict]]]]):
         # your code here
         return image_data
 
-    def hist_equalize(self, image_data: List[Dict[str, Union[str, dict]]]):
+    def hist_equalize(self, image_data: List[Dict[str, Union[str, List[dict]]]]):
         # your code here
         return image_data
 
-    def combine_masks(self, image_data: List[Dict[str, Union[str, dict]]], out_dir: str) -> None:
-        pass
+    def combine_masks(self, image_data: List[Dict[str, Union[str, List[dict]]]]) -> List[Tuple[np.ndarray, str]]:
+        i = 20
+        for record in image_data:
+            image_name, roi_cuts = record['image_name'], record['roi_cuts']
+            image_path = os.path.join(self.images_dir, image_name)
+
+            print(f"ROIs: {len(roi_cuts)}")
+            print(f"Image name: {image_name}")
+
+            whole_image = np.zeros(cv2.imread(image_path).shape[:2])
+
+            print(f"Whole image shape: {whole_image.shape}")
+
+            for cut in roi_cuts:
+                roi = np.array(cut["image"], dtype=np.uint8)
+                roi = np.ones(roi.shape[:2])
+                print(f"ROI shape: {roi.shape}")
+                x, y, w, h, roi = _check_bbox(roi=roi,
+                                              bbox=(cut["x"], cut["y"], cut["width"], cut["height"]),
+                                              image_shape=whole_image.shape)
+                print(f"ROI shape after: {roi.shape}")
+                buffer_image = np.zeros_like(whole_image)
+                print(f"Buffer image shape: {buffer_image.shape}")
+                print(f"x, y, w, h: {x, y, w, h}")
+                buffer_image[y:y+h, x:x+w] = roi
+                whole_image = cv2.bitwise_or(whole_image, buffer_image)
+
+            whole_image[whole_image == 1] = 255
+            plt.title(image_name)
+            plt.imshow(whole_image, cmap='gray')
+            plt.show()
+            i = i-1
+            if i < 1:
+                return
 
     def run(self, json_path: str, operations: List[Callable], out_dir: str, save_steps: bool = False) -> None:
         pass
+
